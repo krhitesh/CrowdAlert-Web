0b9ff47ce077e74c5e3d5feadd060333f97bb746
hiteshk07 <hiteshkr07@bitbucket.org>
2019-04-02 23:36:01 +0530

    testing containerisation

---
+++ env.list.sh
@@ -0,0 +1,18 @@
+heroku config:set REACT_APP_GOOGLE_MAPS_KEY="AIzaSyCBm-Sx5cvVw5gZAGBgHuHG6uT6YVW-QYI" \
+REACT_APP_FACEBOOK_APP_ID="394020658095254" \
+REACT_APP_FIREBASE_API_KEY="AIzaSyBTw4bnH1RUPHqRJnzib11fbnCKYeUdO_0" \
+REACT_APP_FIREBASE_AUTH_DOMAIN="crowdalert-gsoc.firebaseapp.com" \
+REACT_APP_FIREBASE_DATABASE_URL="https://crowdalert-gsoc.firebaseio.com" \
+REACT_APP_FIREBASE_PROJECT_ID="crowdalert-gsoc" \
+REACT_APP_FIREBASE_SENDER_ID="1005599559126" \
+DJANGO_FIREBASE_FCM_SENDER_TOKEN="1005599559126" \
+DJANGO_FIREBASE_type="service_account" \
+DJANGO_FIREBASE_project_id="crowdalert-gsoc" \
+DJANGO_FIREBASE_private_key_id="f756af9efc94ad0a55b1a3fe8f028675a2dfef59" \
+DJANGO_FIREBASE_private_key="-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDy8LSAgaZgjjGC\n+dwbuAA0erT9L1Ohl72bWK1OCQkM50V4rPG2KfKBk/6x2qNft6g6coLA4OiZSVOA\nnKaOYgxb7UjOKJiE5hv4ACrNohTaj0mcJhz1rxSF8y2pLPRx5ySEVEJenu/VITWN\n1xddB8WvU7vb5KvlPZuHZ2tUDVbNInZi/468jP5rlC3seU4SKp+235jVyxmjmnHq\nXlzJC+y0PB0ggauA3bqIpvB3/M/Kn4MokcZIe3LdEDQc4al+SYXnZjwuQY683eU5\nvt3bPF296xg4VRBrsTi0Ab+0pVTPVXG66ln20adLrSDnonGuNbZvmkhSKssxQ0UZ\nVmnw7XBhAgMBAAECgf9eqOusJthtRvqDzle1dTyfNjDPdmFf1YBLx4G/pkWggXzQ\nSJLmJ/IWC1RyeH6wDR+/pl7MSNZY4/llY6SCVjH6n7p+mdLT9dVwDKCg/0ag4rQL\n483k7RNSHi7sfFfDCg1wgmRZ+hP5N7dmTSkNVrnwVvAb/j38VMUFPYbUeV4ZD1I3\n3Q2t/So7Yid5ylFJW0I7kML+p3Xzm5f66WZJfwWZJwou46fVQ907Ar2KYbVPfy6g\nujgaGAYXgV12UQIY5T0BiXHjYpB5BbfnJpUuZjxFwCmHPnZHwmWT2yVB+CrkBy5D\nLmNZpQAmUdN8UDdCQ14pXd+u00Z8pYNpKOsClJUCgYEA+2zMa7OKyUg8Ym5xlrHi\nAFKoTLV3hYOeLwnJlLBGdC4RhJiBbtc1tp763j4yrM3ZmlgGrzuTEP+Y/6xXuEXI\n0z5s6+j/4PrQnPPFDPUAMvwawCwyFiqIfN69doCgySnffiyyTyb2tAOvD9XKCFSt\ncCcRfz21nPHC++arkHfkbm0CgYEA91xh6xrdZLEDZTJ2mk6DiLTeIPv4lcdEpXVr\nYtrNqQssKpjdXNBbKH7YaqKzCsE4Ij003b54a6d/A8LXAsP6YhIh3FUXdGDyfG9f\n3immPtjyhD5sJS38Ai+5xL2q6a3w9LwgjubeipYWgn3rJINKWwH/ZDiEr7FD+X3u\nqEZaQUUCgYAU2M6SV464Zc7r8nBkqFcu46ukwJ8qaOFi0RLvyzXsSC2XqvWAdUO0\n4rW2W73j6iWS0J2tRelJzYeoG2gilVazfUeWv/kHdIR7nxLFokLj91dYe9GjWN98\nSxO2ka/DPj+TvK0ENxzZjDyluXpI+oxu8i5XOIyoeLtMG7I6BlbgeQKBgQDIAfPO\nxP8kjph/N+l8ZWWnoQ9GvlZGzUPtQV8eJGtMdxM0Mjwy8unyeXYF6x434PnBfZoE\nfiEicuGZqe5udwY6TA0C+e6oFEedY/SxTx30DTkywsJw4Zi2L97WfMH4hsOhL4hn\n47cneaojwyrhcCMsKRZW8/gdk4ix8CiL+a66vQKBgQDCumR11PZ/nkTNUcCugYJs\nth5vZ7Cw58TF2zzB1xPsdy9OUPHailwMQMn4J1Ef8z3KR4wrSIHcuenC9rfZ6Sd5\nt0mz/Wnf8oIEB3xnOCXEDDVtHi5XB0yX2GfRv2dYDRXNEPqGTHkHSp9n4nX7GhMa\nh/6pktN+JG+FtDrwTevRxQ="="\n-----END PRIVATE KEY-----\n" \
+DJANGO_FIREBASE_client_email="firebase-adminsdk-7qjf3@crowdalert-gsoc.iam.gserviceaccount.com" \
+DJANGO_FIREBASE_client_id="109351713561769767450" \
+DJANGO_FIREBASE_auth_uri="https://accounts.google.com/o/oauth2/auth" \
+DJANGO_FIREBASE_token_uri="https://oauth2.googleapis.com/token" \
+DJANGO_FIREBASE_auth_provider_x509_cert_url="https://www.googleapis.com/oauth2/v1/certs" \
+DJANGO_FIREBASE_client_x509_cert_url="https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-7qjf3%40crowdalert-gsoc.iam.gserviceaccount.com"
