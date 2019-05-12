294d2763ec811cd324dc3a5ee4e58f7655c654d8
hiteshk07 <hiteshkr07@bitbucket.org>
2019-05-12 23:32:10 +0530

    Added build configurations 	- Webpack server bundle.js 	- Webpack client bundle.js

--- webpack.client.js
+++ webpack.client.js
@@ -1,9 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
-const merge = require('webpack-merge');
-const baseConfig = require('./webpack.base.js');
 
-const config = {
+module.exports = {
   entry: './src/client/client.js',
   output: {
     filename: 'bundle.js',
@@ -12,7 +10,6 @@ const config = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env.BROWSER': JSON.stringify(true),
-      'process.env.REACT_APP_GOOGLE_MAPS_KEY': JSON.stringify(process.env.REACT_APP_GOOGLE_MAPS_KEY),
       'process.env.REACT_APP_FIREBASE_API_KEY': JSON.stringify(process.env.REACT_APP_FIREBASE_API_KEY),
       'process.env.REACT_APP_FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.REACT_APP_FIREBASE_AUTH_DOMAIN),
       'process.env.REACT_APP_FIREBASE_DATABASE_URL': JSON.stringify(process.env.REACT_APP_FIREBASE_DATABASE_URL),
@@ -21,6 +18,41 @@ const config = {
       'process.env.REACT_APP_FACEBOOK_APP_ID': JSON.stringify(process.env.REACT_APP_FACEBOOK_APP_ID),
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loaders: [
+          'babel-loader?presets[]=react,presets[]=es2015,presets[]=stage-0',
+        ],
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: [
+          'babel-loader?presets[]=react,presets[]=es2015,presets[]=stage-0',
+        ],
+      },
+      {
+        test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'isomorphic-style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
 };
-
-module.exports = merge(baseConfig, config);
