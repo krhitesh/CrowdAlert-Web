e310a8bc3a7dd6637a77e61c43a9f1b6a0535ee6
hiteshk07 <hiteshkr07@bitbucket.org>
2019-05-12 23:32:10 +0530

    Added build configurations 	- Webpack server bundle.js 	- Webpack client bundle.js

---
+++ webpack.server.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const webpack = require('webpack');
+const webpackNodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  target: 'node',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.BROWSER': JSON.stringify(false),
+      'process.env.REACT_APP_FIREBASE_API_KEY': JSON.stringify(process.env.REACT_APP_FIREBASE_API_KEY),
+      'process.env.REACT_APP_FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.REACT_APP_FIREBASE_AUTH_DOMAIN),
+      'process.env.REACT_APP_FIREBASE_DATABASE_URL': JSON.stringify(process.env.REACT_APP_FIREBASE_DATABASE_URL),
+      'process.env.REACT_APP_FIREBASE_PROJECT_ID': JSON.stringify(process.env.REACT_APP_FIREBASE_PROJECT_ID),
+      'process.env.REACT_APP_FIREBASE_SENDER_ID': JSON.stringify(process.env.REACT_APP_FIREBASE_SENDER_ID),
+      'process.env.REACT_APP_FACEBOOK_APP_ID': JSON.stringify(process.env.REACT_APP_FACEBOOK_APP_ID),
+    }),
+  ],
+  externals: [webpackNodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loaders: [
+          'babel-loader?presets[]=react,presets[]=es2015,presets[]=stage-0',
+        ],
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: [
+          'babel-loader?presets[]=react,presets[]=es2015,presets[]=stage-0',
+        ],
+      },
+      {
+        test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'isomorphic-style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
