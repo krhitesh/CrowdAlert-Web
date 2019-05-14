7e34d4f0511ecef83b5421a853c07a3fb5904ac5
hiteshk07 <hiteshkr07@bitbucket.org>
2019-05-14 10:57:28 +0530

    Refactor src/ into client/ and index.js and Create index.js for Express server

--- src/index.js
+++ src/index.js
@@ -1,58 +1,18 @@
-/* eslint-disable arrow-body-style */
-/* eslint-disable array-callback-return */
-/* eslint-disable consistent-return */
 /* eslint-disable quotes */
-import 'babel-polyfill';
 import express from 'express';
-import { gzip } from 'zlib';
-import { matchRoutes } from 'react-router-config';
-import Routes from './client/Routes';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
 
 const app = express();
-app.use('*.js', (req, res, next) => {
-  req.url += '.br';
-  res.set('Content-Encoding', 'br');
-  res.set('Content-Type', 'text/javascript');
-  next();
-});
 
 app.use(express.static('public'));
-app.get('*', async (req, res) => {
-  // Use firebase Admin SDK on server to verify
-  // the retrieved ID token. If that ID token is
-  // valid, fetch the user then dispatch appropriate
-  // redux action with that user data.
 
+app.get('**', (req, res) => {
+  // const content = renderToString(main);
 
-  // UserAuth(TODO) -> Dispatch apt action to inform redux (and state) that hey,
-  // "this" is the auth status of the user who requested the application.
-  // Then use the new state to render the application.
+  const html = ``;
 
-  // console.log(matchRoutes(Routes, req.path));
-  const promises = matchRoutes(Routes, req.path).map(({ route, match }) => {
-    // Crank up loadData function from all the 'to be' rendered here and 
-    // return all the dispatches
-    // as promises, when completed will allow us to render the application
-    // on server
-  });
-
-  Promise.all(promises)
-    .then(() => {
-      // Now is the time to render the application
-
-      const content = `<div>Server rendered HTML will come here</div>`;
-
-      gzip(content, (err, gzipped) => {
-        if (err) {
-          // Send the uncompressed content as it is
-          res.send(content);
-        } else {
-          res.set('Content-Encoding', 'gzip');
-          res.set('Content-Type', 'text/html');
-          res.send(gzipped);
-        }
-      });
-    });
+  res.send(html);
 });
 
 app.listen(3000, () => {
