78361b8b506a2c6b9cae1a64cef441362a71a8b6
hiteshk07 <hiteshkr07@bitbucket.org>
2019-05-14 10:57:28 +0530

    Refactor src/ into client/ and index.js and Create index.js for Express server

--- src/client/containers/Feed/index.jsx
+++ src/client/containers/Feed/index.jsx
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import PropTypes from 'prop-types';
 
-import { fetchUserLocation, fetchEventsByLocation, fetchUserLocationSSR, fetchEventsByLocationSSR, fetchEventsByLocationOverWebSocket } from './actions';
+import { fetchUserLocation, fetchEventsByLocation } from './actions';
 import style from './style';
 import { MapWrapper, Sonar, EventPreviewCard, GeoLocator } from '../../components';
-import { DOMAIN_NAME } from '../../utils/apipaths';
-import SEO from '../../components/SEO';
 
 function getEventMarkers(feed, zoom) {
   // Boundary conditions
@@ -29,12 +26,12 @@ function getEventMarkers(feed, zoom) {
   for (let i = 1; i < 18;) {
     if ((zoom + i > 4 && zoom + i < 18)
       && feedData[zoom + i] && feedData[zoom + i].length) {
-      console.log(feedData[zoom + i], i, zoom + i);
+      console.log(feedData[zoom + i], i, zoom+i);
       return feedData[zoom + i] || [];
     }
     if ((zoom - i > 4 && zoom - i < 18)
       && feedData[zoom - i] && feedData[zoom - i].length) {
-      console.log(feedData[zoom - i], i, zoom - i);
+      console.log(feedData[zoom - i], i, zoom-i);
       return feedData[zoom - i];
     }
     i += 1;
@@ -47,43 +44,24 @@ function getEventMarkers(feed, zoom) {
  * @extends Component
  */
 class Feed extends Component {
-  componentDidMount() {
-    if (this.props.isLoggedIn) {
-      this.props.fetchEventsByLocationOverWebSocket({
-        lat: this.props.mapProps.lat,
-        lng: this.props.mapProps.lng,
-        zoom: this.props.mapProps.zoom,
-      });
-    } else {
-      this.props.fetchEventsByLocation({
-        lat: this.props.mapProps.lat,
-        lng: this.props.mapProps.lng,
-        zoom: this.props.mapProps.zoom,
-      });
-    }
+  /**
+   * [componentWillMount fetch  the event as soon as the component will mount]
+   * @return {[type]} [description]
+   */
+  componentWillMount() {
+    // Fetch the users current approximate location using API
+    this.props.fetchUserLocation({
+      oldLat: this.props.mapProps.lat,
+      oldLng: this.props.mapProps.lng,
+    });
+    this.props.fetchEventsByLocation({
+      lat: this.props.mapProps.lat,
+      lng: this.props.mapProps.lng,
+      zoom: this.props.mapProps.zoom,
+    });
   }
-  // eslint-disable-next-line class-methods-use-this
-  head() {
-    let types = [];
-    getEventMarkers(this.props.feedProps, this.props.mapProps.zoom)
-      .forEach((event) => {
-        if (event.isClustered === true) {
-          if (types.indexOf('other') === -1) {
-            types.push('other');
-          }
-        } else if (types.indexOf(event.category) === -1) {
-          types.push(event.category);
-        }
-      });
-
-    const count = types.length;
-    let ogDesc = `${count} incidents of ${types} have been reported in your area. Stay safe.`;
-    if (types.indexOf('other') !== -1) {
-      types = types.filter(type => type !== 'other');
-      ogDesc = `${count}+ incidents of ${types.join(', ')} and others have been reported in your area. Stay safe.`;
-    }
-
-    return <SEO title="Feed | CrowdAlert" url={DOMAIN_NAME} description={ogDesc} />;
+  componentWillUnmount() {
+    console.log('UNMOUNT');
   }
   render() {
     // console.log(this.props);
@@ -102,7 +80,6 @@ class Feed extends Component {
         ));
     return (
       <div style={style}>
-        {this.head()}
         <MapWrapper shouldFetch>
           { Markers }
         </MapWrapper>
@@ -116,52 +93,15 @@ class Feed extends Component {
 const mapStateToProps = (state) => {
   const { map } = state;
   const { feed } = state;
-  const { isLoggedIn } = state.auth;
   return {
     mapProps: map,
     feedProps: feed,
-    isLoggedIn,
   };
 };
-
 const mapDispatchToProps = dispatch => (
   bindActionCreators({
     fetchUserLocation,
     fetchEventsByLocation,
-    fetchEventsByLocationOverWebSocket,
   }, dispatch)
 );
-
-Feed.propTypes = {
-  feedProps: PropTypes.object.isRequired,
-  mapProps: PropTypes.shape({
-    zoom: PropTypes.number,
-    lat: PropTypes.number,
-    lng: PropTypes.number,
-  }).isRequired,
-  fetchEventsByLocation: PropTypes.func.isRequired,
-  fetchEventsByLocationOverWebSocket: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired,
-};
-
-export default {
-  component: connect(mapStateToProps, mapDispatchToProps)(Feed),
-  loadData: (store, ip = '', match = {}) => {
-    const { dispatch } = store;
-
-    return dispatch(fetchUserLocationSSR(
-      {
-        oldLat: -26.77,
-        oldLng: 135.17,
-      },
-      ip,
-    )).then(() => {
-      const { lat, lng, zoom } = store.getState().map;
-      return dispatch(fetchEventsByLocationSSR({
-        lat,
-        lng,
-        zoom,
-      }));
-    });
-  },
-};
+export default connect(mapStateToProps, mapDispatchToProps)(Feed);
