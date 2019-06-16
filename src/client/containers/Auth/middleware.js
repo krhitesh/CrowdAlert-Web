9f588414a701e9943a56f1773498f29a1d64f152
hiteshk07 <hiteshkr07@bitbucket.org>
2019-06-16 13:40:34 +0530

    Replace `window.localStorage` by cookies as required by auth

--- src/client/containers/Auth/middleware.js
+++ src/client/containers/Auth/middleware.js
@@ -90,6 +90,7 @@ const authMiddleware = ({ dispatch }) => next => (action) => {
         Auth.currentUser.getIdToken().then((token) => {
           if (typeof window !== 'undefined') {
             window.sessionStorage.setItem('token', token);
+            document.cookie = `token=${token}`;
           }
         });
         console.log('User Logged IN');
@@ -98,6 +99,8 @@ const authMiddleware = ({ dispatch }) => next => (action) => {
           window.localStorage.setItem('shouldBeLoggedIn', false);
           window.localStorage.removeItem('user');
           window.sessionStorage.removeItem('token');
+
+          document.cookie = 'token=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
         }
         dispatch(updateUserAuthenticationData({
           loggedIn: false,
@@ -185,10 +188,12 @@ const oAuthMiddleware = ({ dispatch }) => next => (action) => {
     window.localStorage.setItem('shouldBeLoggedIn', true);
     switch (action.payload.provider) {
       case 'facebook':
-        Auth.signInWithRedirect(FacebookAuth);
+        Auth.signInWithRedirect(FacebookAuth)
+          .then(result => dispatch(checkUserAuthenticationStatus()));
         break;
       case 'google':
-        Auth.signInWithRedirect(GoogleAuth);
+        Auth.signInWithRedirect(GoogleAuth)
+          .then(result => dispatch(checkUserAuthenticationStatus()));
         break;
       default:
         break;
