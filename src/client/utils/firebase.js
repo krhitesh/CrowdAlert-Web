cde799cee35fbed45fc83f5c2175ea879a5da16b
hiteshk07 <hiteshkr07@bitbucket.org>
2019-06-20 10:51:22 +0530

    - Use of block comments instead of line comments in appropriate places - Removed unused imports, lifecycle functions

--- src/client/utils/firebase.js
+++ src/client/utils/firebase.js
@@ -10,7 +10,7 @@ firebase.initializeApp(firebaseConfig);
 if (typeof window !== 'undefined') {
   window.firebase = firebase;
 }
-// const database = firebase.database();
+
 export const Auth = firebase.auth();
 export const FacebookAuth = new firebase.auth.FacebookAuthProvider();
 export const GoogleAuth = new firebase.auth.GoogleAuthProvider();
--- webpack.server.js
+++ webpack.server.js
@@ -5,9 +5,11 @@ const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
 const webpackNodeExternals = require('webpack-node-externals');
 
-// Brotli compression and therefore generating server bundle.js.br is not required
-// as it is NOT send to client's browser.
-// Remove the brotli compression plugin. It is the last element in the plugins array.
+/*
+Brotli compression and therefore generating server bundle.js.br is not required
+as it is NOT send to client's browser.
+Remove the brotli compression plugin. It is the last element in the plugins array.
+*/
 baseConfig.plugins.pop();
 
 const config = {
