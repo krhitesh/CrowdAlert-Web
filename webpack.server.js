852262c1ec88e7422d9c43994b2a5f5f38aaf084
hiteshk07 <hiteshkr07@bitbucket.org>
2019-06-20 11:12:43 +0530

    Removed .env file dependency in webpack.server.js

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const Dotenv = require('dotenv-webpack');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
 const webpackNodeExternals = require('webpack-node-externals');
@@ -22,9 +21,25 @@ const config = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env.BROWSER': JSON.stringify(false),
-    }),
-    new Dotenv({
-      path: './.env',
+      'process.env.REACT_APP_GOOGLE_MAPS_KEY': JSON.stringify(process.env.REACT_APP_GOOGLE_MAPS_KEY),
+      'process.env.REACT_APP_FIREBASE_API_KEY': JSON.stringify(process.env.REACT_APP_FIREBASE_API_KEY),
+      'process.env.REACT_APP_FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.REACT_APP_FIREBASE_AUTH_DOMAIN),
+      'process.env.REACT_APP_FIREBASE_DATABASE_URL': JSON.stringify(process.env.REACT_APP_FIREBASE_DATABASE_URL),
+      'process.env.REACT_APP_FIREBASE_PROJECT_ID': JSON.stringify(process.env.REACT_APP_FIREBASE_PROJECT_ID),
+      'process.env.REACT_APP_FIREBASE_SENDER_ID': JSON.stringify(process.env.REACT_APP_FIREBASE_SENDER_ID),
+      'process.env.REACT_APP_FACEBOOK_APP_ID': JSON.stringify(process.env.REACT_APP_FACEBOOK_APP_ID),
+
+      'process.env.DJANGO_FIREBASE_FCM_SENDER_TOKEN': JSON.stringify(process.env.DJANGO_FIREBASE_FCM_SENDER_TOKEN),
+      'process.env.DJANGO_FIREBASE_type': JSON.stringify(process.env.DJANGO_FIREBASE_type),
+      'process.env.DJANGO_FIREBASE_project_id': JSON.stringify(process.env.DJANGO_FIREBASE_project_id),
+      'process.env.DJANGO_FIREBASE_private_key_id': JSON.stringify(process.env.DJANGO_FIREBASE_private_key_id),
+      'process.env.DJANGO_FIREBASE_private_key': JSON.stringify(process.env.DJANGO_FIREBASE_private_key).replace(/\\n/g, '\n'),
+      'process.env.DJANGO_FIREBASE_client_email': JSON.stringify(process.env.DJANGO_FIREBASE_client_email),
+      'process.env.DJANGO_FIREBASE_client_id': JSON.stringify(process.env.DJANGO_FIREBASE_client_id),
+      'process.env.DJANGO_FIREBASE_auth_uri': JSON.stringify(process.env.DJANGO_FIREBASE_auth_uri),
+      'process.env.DJANGO_FIREBASE_token_uri': JSON.stringify(process.env.DJANGO_FIREBASE_token_uri),
+      'process.env.DJANGO_FIREBASE_auth_provider_x509_cert_url': JSON.stringify(process.env.DJANGO_FIREBASE_auth_provider_x509_cert_url),
+      'process.env.DJANGO_FIREBASE_client_x509_cert_url': JSON.stringify(process.env.DJANGO_FIREBASE_client_x509_cert_url),
     }),
   ],
   externals: [webpackNodeExternals()],
