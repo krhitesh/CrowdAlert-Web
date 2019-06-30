d8dd41d02afa6f733b909e8e4b0b43cdb1db58af
hiteshk07 <hiteshkr07@bitbucket.org>
2019-06-30 20:29:52 +0530

    Finished websockets for Events, Comments using in memory channel layer 	- Create routing.py, the file that contains web socket routing information 	- Writing comments consumer

--- api/comments/consumers.py
+++ api/comments/consumers.py
@@ -1,60 +1,57 @@
-import json
-
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
-
+import json
 
 class CommentsConsumer(WebsocketConsumer):
+  """
+  Change this consumer to AsyncWebsocketConsumer
+
+  """
+  def connect(self):
     """
-    Change this consumer to AsyncWebsocketConsumer
+    Adds current channel to the room group of the name 'comments_{room_name}' where
+    room name is the thread_id. Finally it accepts the websocket connection
 
     """
+    self.room_name = self.scope['url_route']['kwargs']['thread_id']
+    self.room_group_name = 'comments_%s' % self.room_name
 
-    def connect(self):
-        """
-        Adds current channel to the room group of the name 'comments_{room_name}' where
-        room name is the thread_id. Finally it accepts the websocket connection
+    # Join room group
+    async_to_sync(self.channel_layer.group_add)(
+        self.room_group_name,
+        self.channel_name
+    )
 
-        """
-        self.room_name = self.scope['url_route']['kwargs']['thread_id']
-        self.room_group_name = 'comments_%s' % self.room_name
+    self.accept()
 
-        # Join room group
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name,
-            self.channel_name
-        )
+  def disconnect(self, close_code):
+    """
+    Disconnects the client of the current channel. Also removes current channel
+    from the room group of the name 'comments_{room_name}' where
+    room name is the thread_id.
 
-        self.accept()
+    """
+    async_to_sync(self.channel_layer.group_discard)(
+        self.room_group_name,
+        self.channel_name
+    )
 
-    def disconnect(self, close_code):
-        """
-        Disconnects the client of the current channel. Also removes current channel
-        from the room group of the name 'comments_{room_name}' where
-        room name is the thread_id.
+  def receive(self, text_data):
+    pass
 
-        """
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name,
-            self.channel_name
-        )
+  def comments_message(self, event):
+    """
+    Handler function to send data from server to client in an open
+    websocket connection.
 
-    def receive(self, text_data=None, bytes_data=None):
-        pass
+    For example, inside an async function from anywhere in the app:
+    await channel_layer.send({
+      'type': 'comments_message',
+      'message': 'your data'
+    })
 
-    def comments_message(self, event):
-        """
-        Handler function to send data from server to client in an open
-        websocket connection.
+    """
+    data = event['message']
 
-        For example, inside an async function from anywhere in the app:
-        await channel_layer.send({
-          'type': 'comments_message',
-          'message': 'your data'
-        })
-
-        """
-        data = event['message']
-
-        # Finally shovel data to the client
-        self.send(text_data=json.dumps(data))
+    # Finally shovel data to the client
+    self.send(text_data=json.dumps(data))
