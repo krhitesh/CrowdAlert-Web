e50807496a4ff21f0acd127727ffa84a7c0230cf
hiteshk07 <hiteshkr07@bitbucket.org>
2019-06-30 20:29:52 +0530

    Finished websockets for Events, Comments using in memory channel layer 	- Create routing.py, the file that contains web socket routing information 	- Writing comments consumer

--- CrowdAlert/routing.py
+++ CrowdAlert/routing.py
@@ -2,13 +2,15 @@ from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
 from channels.routing import ProtocolTypeRouter, URLRouter
 from api.events import routing as events_routing
+from api.comments import routing as comments_routing
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
     'websocket': AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter(
-              events_routing.websocket_urlpatterns
+              events_routing.websocket_urlpatterns + 
+              comments_routing.websocket_urlpatterns
             )
         )
     )
