e28b02d84cf9215f7aab56b0997778b21e3f6bc3
hiteshk07 <hiteshkr07@bitbucket.org>
2019-07-03 21:16:39 +0530

    Added firestore in django app settings

--- CrowdAlert/settings.py
+++ CrowdAlert/settings.py
@@ -15,8 +15,6 @@ import os
 
 import dj_database_url
 import firebase_admin
-import googlemaps
-from api.utils import pyrebase
 from firebase_admin import firestore
 
 # Generate the Firebase Service Account Credential json file
