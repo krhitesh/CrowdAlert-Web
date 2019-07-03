32c44ada5010f49e87cac5c8681945f3dc712b36
hiteshk07 <hiteshkr07@bitbucket.org>
2019-07-03 21:25:26 +0530

    Added spam model

--- api/spam/models.py
+++ api/spam/models.py
@@ -1,46 +1,39 @@
-from google.cloud.firestore_v1beta1 import ArrayRemove
-
+from google.cloud.firestore_v1beta1 import ArrayUnion, ArrayRemove
 
 class Classifier(object):
-    collection_name = 'classifiers'
+  collection_name = 'classifiers'
 
-    def __init__(self, flag_count, flag_users, toxic=None):
-        if toxic is None:
-            toxic = {}
-        self.flag_count = flag_count
-        self.flag_users = flag_users
-        self.toxic = toxic
+  def __init__(self, flag_count, flag_users):
+    self.flag_count = flag_count
+    self.flag_users = flag_users
 
-    def save(self, uuid, db):
-        db.collection(Classifier.collection_name).document(uuid).set(self.to_dict())
-        return uuid
+  def save(self, incident_id, db):
+    db.collection(Classifier.collection_name).document(incident_id).set(self.to_dict())
+    return incident_id
 
-    def update(self, new_flag_count, flag_user, uuid, db):
-        self.flag_users.append(flag_user)
-        self.flag_count = new_flag_count
-        db.collection(Classifier.collection_name).document(uuid).update({
-            u"flag_users": ArrayRemove([flag_user]),
-            u"flag_count": new_flag_count
-        })
+  def update(self, new_flag_count, flag_user, incident_id, db):
+    self.flag_users.append(flag_user)
+    self.flag_count = new_flag_count
+    db.collection(Classifier.collection_name).document(incident_id).update({
+      u"flag_users": ArrayRemove([flag_user]),
+      u"flag_count": new_flag_count
+    })
 
-    @staticmethod
-    def get(uuid, db):
-        doc = db.collection(Classifier.collection_name).document(uuid).get()
-        if doc.exists:
-            return Classifier.from_dict(doc.to_dict())
-        else:
-            return None
+  @staticmethod
+  def get(incident_id, db):
+    doc = db.collection(Classifier.collection_name).document(incident_id).get()
+    return Classifier.from_dict(doc.to_dict())
 
-    @staticmethod
-    def from_dict(source_dict):
-        classifier = Classifier(source_dict['flag_users'], source_dict['flag_users'], source_dict['toxic'])
-        return classifier
+  @staticmethod
+  def from_dict(source_dict):
+    classifier = Classifier(source_dict['flag_users'], source_dict['flag_users'])
+    return classifier
 
-    def to_dict(self):
-        classifier_dict = {
-            'flag_users': self.flag_users,
-            'flag_count': self.flag_count,
-            'toxic': self.toxic
-        }
+  def to_dict(self):
+    classifier_dict = {
+      'flag_users': self.flag_users,
+      'flag_count': self.flag_count
+    }
 
-        return classifier_dict
+    return classifier_dict
+    
