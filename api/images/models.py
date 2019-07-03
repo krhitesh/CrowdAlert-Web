befb84f3c028a2bd0bd10a7d764324b3528a557b
hiteshk07 <hiteshkr07@bitbucket.org>
2019-07-03 21:25:11 +0530

    Added images models

--- api/images/models.py
+++ api/images/models.py
@@ -4,14 +4,10 @@ But we can replace the firestore mechanism with a custom model
 import os
 import subprocess
 import time
-import requests
-import json
 from threading import Thread
-
+from api.events.models import Event
 from google.cloud.firestore_v1beta1 import ArrayUnion, ArrayRemove
 
-from api.events.models import Event
-
 def asyncfunc(function):
     """ Wrapper for async behaviour. Executes function in a separate new thread
     """
@@ -22,20 +18,9 @@ def asyncfunc(function):
         threads.start()
     return decorated_function
 
-url = 'https://test-nsfw-7.herokuapp.com'
-
-
-def asyncfunc(function):
-    """ Wrapper for async behaviour. Executes function in a separate new thread
-    """
-
-    def decorated_function(*args, **kwargs):
-        threads = Thread(target=function, args=args, kwargs=kwargs)
-        # Make sure thread doesn't quit until everything is finished
-        threads.daemon = False
-        threads.start()
-
-    return decorated_function
+@asyncfunc
+def create_thumbnail(name, storage):
+    """ Starts the job of creating svg based thumbnail for a given file
 
     Arguments:
         name {string} -- [ target file name ]
@@ -69,44 +54,10 @@ class Image(object):
         return self.uuid
 
     def put(self, storage):
-        storage.child(Image.path + self.uuid).put(self.name, content_type="image/jpg")
+        storage.child(Image.path + self.uuid).put(self.name)
 
     def create_thumbnail(self, storage):
-        self.__create_thumbnail__(self.name, storage)
-
-    @asyncfunc
-    def run_nsfw_classifier(self, incident_id, db):
-        try:
-            res = requests.post(url, data={'url': 'https://crowdalert.herokuapp.com/api/images/image?uuid=' + self.uuid})
-            score = float(res.text)*100
-            print(score)
-            if score > 20.0:
-                db.collection(Event.collection_name).document(incident_id).update({u'images': ArrayRemove([self.to_dict()])})
-                self.is_nsfw = True
-                db.collection(Event.collection_name).document(incident_id).update({u'images': ArrayUnion([self.to_dict()])})
-        except:
-            pass
-    
-    @asyncfunc
-    def __create_thumbnail__(self, name, storage):
-        """ Starts the job of creating svg based thumbnail for a given file
-
-        Arguments:
-            name {string} -- [ target file name ]
-        """
-        # As our load is small now, we can do this in sequential manner
-        # After we get enough traffic we should use a redis based solution.
-        # Where an event would be pushed and a job id is to be returned
-        # and expose another endpoint where we can check the status
-        print("Generating Thumbnail", time.time())
-        subprocess.run(['node_modules/.bin/sqip', name, '-o', name + '.svg'])
-        storage.child('thumbnails/' + name + '.svg').put(name + '.svg', content_type="image/svg+xml")
-        # Remove the uploaded files for two good reasons:
-        # Keep our dyno clean
-        # remove malicious code before anything goes wrong.
-        os.remove(name)
-        os.remove(name + '.svg')
-        print("Finished", time.time())
+        create_thumbnail(self.name, storage)
 
     @staticmethod
     def from_dict(source_dict):
@@ -120,3 +71,4 @@ class Image(object):
             "uuid": self.uuid,
         }
         return image_dict
+        
