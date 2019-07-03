190fa154746d316f37a9d24581aa955dec73af9f
hiteshk07 <hiteshkr07@bitbucket.org>
2019-07-03 21:26:05 +0530

    Added user model

--- api/users/models.py
+++ api/users/models.py
@@ -1,44 +1,39 @@
 """ Django models for users app
 """
 
-
 class User(object):
-    collection_name = 'users'
+  collection_name = 'users'
 
-    def __init__(self, uid, display_name, photo_url, home_location={}):
-        self.uid = uid
-        self.display_name = display_name
-        self.photo_url = photo_url
-        self.home_location = home_location
+  def __init__(self, uid, display_name, photo_url):
+    self.uid = uid
+    self.display_name = display_name
+    self.photo_url = photo_url
 
-    @staticmethod
-    def get(uid, db):
-        doc = db.collection(User.collection_name).document(uid).get()
-        if doc.exists:
-            return User.from_dict(uid, doc.to_dict())
-        else:
-            return None
+  @staticmethod
+  def get(uid, db):
+    doc = db.collection(User.collection_name).document(uid).get()
+    if doc.exists:
+      return User.from_dict(uid, doc.to_dict())
+    else:
+      return None
 
-    def save(self, db):
-        db.collection(User.collection_name).document(self.uid).set(self.to_dict())
-        return self.uid
+  def save(self, db):
+    db.collection(User.collection_name).document(self.uid).set(self.to_dict())
+    return self.uid
 
-    def update(self, display_name, db):
-        self.display_name = display_name
-        db.collection(User.collection_name).document(self.uid).update({u"displayName": display_name})
+  def update(self, display_name, db):
+    self.display_name =display_name
+    db.collection(User.collection_name).document(self.uid).update({u"displayName", display_name})
 
-    @staticmethod
-    def from_dict(uid, source_dict):
-        user = User(uid, source_dict['displayName'], source_dict['photoURL'])
-        if 'home_location' in source_dict.keys():
-            user.home_location = source_dict['home_location']
-        return user
+  @staticmethod
+  def from_dict(uid, source_dict):
+    user = User(uid, source_dict['displayName'], source_dict['photoURL'])
+    return user
 
-    def to_dict(self):
-        user_dict = {
-            'displayName': self.display_name,
-            'photoURL': self.photo_url,
-            'home_location': self.home_location
-        }
+  def to_dict(self):
+    user_dict = {
+      'displayName': self.display_name,
+      'photoURL': self.photo_url
+    }
 
-        return user_dict
+    return user_dict
