c9047c74629f45c9d88b33f52b341cfc76cfac48
hiteshk07 <hiteshkr07@bitbucket.org>
2019-07-09 10:43:15 +0530

    Minor refactor for testing

---
+++ src/client/index.js
@@ -0,0 +1,52 @@
+/* eslint-disable react/jsx-filename-extension */
+// Startup point for the client side application
+/**
+ * CrowdAlert
+ * index.js: main entry point of the app
+ * eslint is disabled as there are references to window & documnet object.
+ */
+
+import 'babel-polyfill';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import { renderRoutes } from 'react-router-config';
+import StyleContext from 'isomorphic-style-loader/StyleContext';
+import Routes from './Routes';
+import history from '../helpers/history';
+
+import configureStore from './configureStore';
+
+/**
+ * [initialState initial state for the App]
+ * @type {Object}
+ */
+const initialState = window.__INITIAL_STATE__;
+
+delete window.__INITIAL_STATE__;
+
+/**
+ * [store contains the redux store for the app]
+ * @type {[type]}
+ */
+const store = configureStore(initialState, history);
+
+const insertCss = (...styles) => {
+  const removeCss = styles.map(style => style._insertCss());
+  return () => removeCss.forEach(dispose => dispose());
+};
+
+const Client = () => (
+  <StyleContext.Provider value={{ insertCss }}>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <div>{renderRoutes(Routes)}</div>
+      </ConnectedRouter>
+    </Provider>
+  </StyleContext.Provider>
+);
+
+export default {
+  Component: Client,
+  store,
+};
--- src/client/index.test.js
+++ src/client/index.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import client from './index';
 
-it('renders without crashing', () => {
+test('renders without error', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<client.Component />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
